d797cb55188db81fbb978923aa3f34fb99140a80
congzw <2981810300@qq.com>
2020-07-03 18:01:36 +0800

    add IClientMonitor

---
+++ src/SmartClass.Common/ScopedHub/ClientMonitors/IClientMonitor.cs
@@ -0,0 +1,8 @@
+﻿using SmartClass.Common.ScopedHub.ClientMonitors.ClientConnections;
+
+namespace SmartClass.Common.ScopedHub.ClientMonitors
+{
+    public interface IClientMonitor : IClientConnectionManager//, IScopeGroupManager, IClientGroupManager, IClientMethodManager, IClientStubManager
+    {
+    }
+}
