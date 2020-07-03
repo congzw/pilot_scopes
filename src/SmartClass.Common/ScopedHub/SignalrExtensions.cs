c630ae200ec9f585ff783d4b561e1d71e3d16dbc
congzw <2981810300@qq.com>
2020-07-03 14:58:57 +0800

    add ext

---
+++ src/SmartClass.Common/ScopedHub/SignalrExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SmartClass.Common.ScopedHub
+{
+    public static class SignalRExtensions
+    {
+        public static HttpContext TryGetHttpContext(this Hub hub)
+        {
+            return hub?.Context?.GetHttpContext();
+        }
+    }
+}
