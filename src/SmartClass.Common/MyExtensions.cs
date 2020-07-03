8ddb60a8516bc68d3a5a3f9f4048f35ada15cf5a
congzw <2981810300@qq.com>
2020-07-03 15:23:02 +0800

    add ext

---
+++ src/SmartClass.Common/MyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmartClass.Common
+{
+    public static partial class MyExtensions
+    {
+        public static bool MyEquals(this string value, string value2, StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+        {
+            var valueFix = string.Empty;
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                valueFix = value.Trim();
+            }
+
+            var value2Fix = string.Empty;
+            if (!string.IsNullOrWhiteSpace(value2))
+            {
+                value2Fix = value2.Trim();
+            }
+
+            return valueFix.Equals(value2Fix, comparison);
+        }
+    }
+}
