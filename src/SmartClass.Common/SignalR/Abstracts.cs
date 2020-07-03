8ccbd2b915369ca28c59a119350472acd28782f3
congzw <2981810300@qq.com>
2020-07-03 13:53:37 +0800

    add abstract for scope clients

---
+++ src/SmartClass.Common/SignalR/Abstracts.cs
@@ -0,0 +1,47 @@
+﻿namespace SmartClass.Common.SignalR
+{
+    public interface IScopeKey
+    {
+        /// <summary>
+        /// 连接端业务范围的唯一标识，例如某堂课的Id等场景。逻辑上划分的Hub范围
+        /// </summary>
+        string ScopeId { get; set; }
+    }
+
+    public interface IClientKey
+    {
+        /// <summary>
+        /// 连接端Client的唯一标识，例如某设备的Id
+        /// </summary>
+        string ClientId { get; set; }
+    }
+
+    public interface IGroupKey
+    {
+        /// <summary>
+        /// 组名
+        /// </summary>
+        string Group { get; set; }
+    }
+
+    public interface IScopeClientKey : IScopeKey, IClientKey
+    {
+    }
+    
+    public interface IConnectionKey
+    {
+        string ConnectionId { get; set; }
+    }
+
+    public interface IScopeClientConnectionKey : IScopeClientKey, IConnectionKey
+    {
+    }
+
+    public interface IScopeGroupKey : IScopeKey, IGroupKey
+    {
+    }
+
+    public interface IScopeClientGroupKey : IScopeKey, IClientKey, IGroupKey, IScopeClientKey, IScopeGroupKey
+    {
+    }
+}
