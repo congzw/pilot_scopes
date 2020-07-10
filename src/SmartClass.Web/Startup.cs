29a0d1a1af241df214196570f72e56d758dd008e
longshenzhu <1223006618@qq.com>
2020-07-10 13:35:05 +0800

    save

--- src/SmartClass.Web/Startup.cs
+++ src/SmartClass.Web/Startup.cs
@@ -20,7 +20,7 @@ namespace SmartClass.Web
         {
             var fileServerOptions = new FileServerOptions();
             var defaultPages = new List<string>();
-            defaultPages.Add("ClientMonitors/demo/index.html");
+            defaultPages.Add("ClientMonitors/demo/SignalrMonitor.html");
             fileServerOptions.DefaultFilesOptions.DefaultFileNames = defaultPages;
 
             app.UseFileServer(fileServerOptions);
