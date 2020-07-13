b485571bf0ded3770d7eeb5a007ce3d3bbd69e8f
congzw <2981810300@qq.com>
2020-07-13 11:37:49 +0800

    remove no use codes

--- src/SmartClass.Common/ScopeHubs/SignalrExtensions.cs
+++ src/SmartClass.Common/ScopeHubs/SignalrExtensions.cs
@@ -9,63 +9,11 @@ namespace SmartClass.Common.ScopeHubs
         {
             return hub?.Context?.GetHttpContext();
         }
-
-        private static string TryGetScopeId(this Hub hub)
-        {
-            //todo: read from token
-            return hub.TryGetHttpContext().TryGetQueryParameterValue(HubConst.Args_ScopeId, HubConst.ScopeId_Default);
-        }
-
-        private static string TryGetClientId(this Hub hub)
-        {
-            //todo: read from token
-            ////post args : {"scopeId" : "abc"}
-            //var user = hub.Context.User;
-            //var identityName = user.Identity.Name;
-            //var scopeId = user.Claims;
-            var clientId = hub.TryGetHttpContext().TryGetQueryParameterValue(HubConst.Args_ClientId, (string)null);
-            return clientId;
-        }
-
-        private static string TryGetClientType(this Hub hub)
-        {
-            //todo: read from token
-            return string.Empty;
-        }
-
-        //public static SignalREventCallingContext GetSignalREventContext(this HttpContext httpContext)
-        //{
-        //    var scopeId = httpContext.TryGetQueryParameterValue(HubConst.Args_ScopeId, HubConst.ScopeId_Default);
-        //    var clientId = httpContext.TryGetQueryParameterValue(HubConst.Args_ClientId, string.Empty);
-        //    var userId = string.Empty;
-        //    var clientType = httpContext.TryGetQueryParameterValue(HubConst.Args_ClientType, string.Empty);
-
-        //    ////todo: read from claim
-        //    //var user = httpContext.User;
-        //    //var scopeId = user.FindFirst("ScopeId").Value;
-        //    //var clientId = user.FindFirst("ClientId").Value;
-        //    //var clientType = user.FindFirst("ClientType").Value;
-        //    //var userId = user.Claims.Single(x => x.Type == ClaimTypes.Name).Value;
-
-        //    var ctx = new SignalREventCallingContext();
-        //    ctx.ScopeId = scopeId;
-        //    ctx.ClientId = clientId;
-        //    ctx.UserId = userId;
-        //    ctx.ClientType = clientType;
-
-        //    return ctx;
-        //}
-
-        //public static SignalREventCallingContext GetSignalREventContext(this Hub hub)
-        //{
-        //    return hub.TryGetHttpContext().GetSignalREventContext();
-        //}
-        
         public static THub FixScopeIdForArgs<THub>(this THub hub, IScopeKey args) where THub : Hub
         {
             if (string.IsNullOrWhiteSpace(args.ScopeId))
             {
-                args.ScopeId = hub.TryGetScopeId();
+                args.ScopeId = hub.GetSendFrom().ScopeId;
             }
             return hub;
         }
