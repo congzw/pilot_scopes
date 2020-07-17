5188623dcfbcef713a4a198ecbc38369c46f1a52
congzw <2981810300@qq.com>
2020-07-17 11:47:55 +0800

    refact handler

--- src/SmartClass.Common/ScopeHubs/ClientMonitors/ClientConnections/IClientConnectionManager.cs
+++ src/SmartClass.Common/ScopeHubs/ClientMonitors/ClientConnections/IClientConnectionManager.cs
@@ -27,6 +27,51 @@ namespace SmartClass.Common.ScopeHubs.ClientMonitors.ClientConnections
             Reason = reason;
         }
     }
+    
+    public class OnConnectedEventHandler : ISignalREventHandler
+    {
+        private readonly IClientMonitor _clientMonitor;
+
+        public OnConnectedEventHandler(IClientMonitor clientMonitor)
+        {
+            _clientMonitor = clientMonitor;
+        }
+
+        public float HandleOrder { get; set; }
+        public bool ShouldHandle(ISignalREvent @event)
+        {
+            return @event is OnConnectedEvent;
+        }
+
+        public async Task HandleAsync(ISignalREvent @event)
+        {
+            var theEvent = (OnConnectedEvent)@event;
+            await _clientMonitor.OnConnected(theEvent).ConfigureAwait(false);
+        }
+    }
+
+    public class OnDisconnectedEventHandler : ISignalREventHandler
+    {
+        private readonly IClientMonitor _manager;
+
+        public OnDisconnectedEventHandler(IClientMonitor manager)
+        {
+            _manager = manager;
+        }
+
+        public float HandleOrder { get; set; }
+
+        public bool ShouldHandle(ISignalREvent hubEvent)
+        {
+            return hubEvent is OnDisconnectedEvent;
+        }
+
+        public Task HandleAsync(ISignalREvent hubEvent)
+        {
+            var theEvent = (OnDisconnectedEvent)hubEvent;
+            return _manager.OnDisconnected(theEvent);
+        }
+    }
 
     #endregion
 }
